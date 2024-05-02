a2768589c2348d4d89f0ab6cc48b1fc7af9eb609
fatma2090 <boudayafatima@gmail.com>
2024-05-02 18:51:04 +0200

    master

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
-
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length++;
 }
+
+get(pos) {
+  if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+  }
+  return this.items[pos];
+}
+
+
+max() {
+  if (this.length === 0) {
+      throw new Error("EmptySortedList");
+  }
+  return this.items[this.length - 1];
+}
+
+min() {
+  if (this.length === 0) {
+      throw new Error("EmptySortedList");
+  }
+  return this.items[0];
+}
+
+sum() {
+  return this.items.reduce((acc, curr) => acc + curr, 0);
+}
+
+
+avg() {
+  if (this.length === 0) {
+      throw new Error("EmptySortedList");
+  }
+  return this.sum() / this.length;
+}
+}
+
 
 module.exports = SortedList;
